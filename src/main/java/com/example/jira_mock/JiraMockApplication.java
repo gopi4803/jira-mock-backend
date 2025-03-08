587ea38da30662bd6009fc719deea0361af1b187
gopi4803 <chandugc48@gmail.com>
2025-03-08 21:41:53 +0530

    Initial commit with project setup

---
+++ src/main/java/com/example/jira_mock/JiraMockApplication.java
@@ -0,0 +1,13 @@
+package com.example.jira_mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraMockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jira_mock/JiraMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jira_mock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
